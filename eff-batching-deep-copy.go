23902cd3c6dc1a1d4e8c2cc3ff1531a71654a965
Rohil Pal <rohilpal9763@gmail.com>
2025-02-23 16:21:35 +0530

    Add descriptions for each of the approaches

--- eff-batching-deep-copy.go
+++ eff-batching-deep-copy.go
@@ -1,1 +1,4 @@
 package main
+
+// in this approach, we will create a deep copy of the hashmap and then use this deep copy to write into the db
+// after creating a copy we will clear the original map and then start consuming messages again
--- naive-batching-counter.go
+++ naive-batching-counter.go
@@ -1,1 +1,5 @@
 package main
+
+// in this approach, we will locally keep a copy of the counters of each tag
+// and only after a certain time will write it to mongodb
+// a local copy can be kept in a hashmap
